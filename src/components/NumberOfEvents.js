5a5124b1b05e3adf9b155387650b4654af777f87
javapak24 <ajava2424@gmail.com>
2024-08-07 18:47:03 -0400

    noe field works

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function NumberOfEvents() {
+export default function NumberOfEvents({currentNOE , setCurrentNOE}) {
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setCurrentNOE(value);
+  }
   return (
     <div>
- <input type="text" role='input'></input>
+ <input type="text" role='input' onChange={handleInputChanged}></input>
     </div>
   )
 }
