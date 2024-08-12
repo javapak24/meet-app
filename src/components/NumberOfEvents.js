8f7e008e3baf057ef2c25f0a96c803542124a938
javapak24 <ajava2424@gmail.com>
2024-08-12 19:37:46 -0400

    4.8

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function NumberOfEvents({currentNOE , setCurrentNOE}) {
+export default function NumberOfEvents({currentNOE , setCurrentNOE, setErrorAlert}) {
+  const [error, setError] = useState('');
   const handleInputChanged = (event) => {
     const value = event.target.value;
+
+    if (isNaN(value) || value < 0){
+      setError('Invalid numbers')
+    }
+    else {
+      setError("")
+    }
+    setErrorAlert(error);
     setCurrentNOE(value);
   }
   return (
