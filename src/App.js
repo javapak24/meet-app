8fddb15bcab42efda3fc99e0523203048e43e4c8
javapak24 <ajava2424@gmail.com>
2024-08-15 14:33:58 -0400

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert,ErrorAlert } from './components/Alert';
+import { InfoAlert,ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
 import CityEventsChart from './components/CityEventsChart';
 
@@ -14,6 +14,8 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
+
 
 
 
@@ -35,6 +37,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents currentNOE = {currentNOE} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
