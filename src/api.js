38220fe3884b27797e2e03e4d70064422de1b409
javapak24 <ajava2424@gmail.com>
2024-08-15 14:17:11 -0400

    test

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from './mock-data';
+import NProgress from "nprogress";
 
 /**
  *
@@ -14,10 +15,99 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+
+
+export const getAccessToken = async () => {             
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://ednvy55cy9.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+ 
+    const url = "https://ednvy55cy9.execute-api.eu-central-1.amazonaws.com/dev/api/token";
+    // eslint-disable-next-line no-useless-concat
+
+    const getUrl = `${url}` + "/" + `${encodeCode}`;
+    const response = await fetch(getUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    return error;
+  }
+}; 
+
+
+export const getEvents = async () => {
+  NProgress.start();
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
+  const token = await getAccessToken();
+  const url =  "https://ednvy55cy9.execute-api.eu-central-1.amazonaws.com/dev/api/get-events";
+
+  if (token) {
+    // removeQuery();
+    const eventsURL = url + "/" + token;
+    const response = await fetch(eventsURL)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data) return [];
+        NProgress.done();
+        localStorage.setItem("lastEvents", JSON.stringify(data));
+        //access data directly instead not data.events
+        const result = data;
+        return result;
+      }
+    ).catch((error) => {
+      if (error) {
+        NProgress.done();
+        throw new Error(`HTTP error! status: ${error.status}`);
+      }
+      console.log(error);
+    });
+    
+    return response;
+  }
+};
+
 /**
  *
  * This function will fetch the list of all events
  */
-export const getEvents = async () => {
-  return mockData;
-};
