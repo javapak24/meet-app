d5c3f7f8f68f857f436a1e5e3926e588dfee46cc
javapak24 <ajava2424@gmail.com>
2024-08-19 19:43:09 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -32,11 +32,12 @@ const App = () => {
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
+    return allEvents;
+    // const filteredEvents = currentCity === "See all cities" ?
+    //   allEvents :
+    //   allEvents.filter(event => event.location === currentCity)
+    // setEvents(filteredEvents.slice(0, currentNOE));
+    // setAllLocations(extractLocations(allEvents));
   }
 
   return (
