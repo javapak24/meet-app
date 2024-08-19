deea3d295a5ddc440550334d6381fc722b75f0f0
javapak24 <ajava2424@gmail.com>
2024-08-19 19:34:11 -0400

    fix ggg

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,7 +5,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://javapak24.github.io/meet/"
+  "https://javapak24.github.io/meet-app/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
