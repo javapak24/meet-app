5963272d2e7d2f5eceee4a4fa64198908a49b4ef
javapak24 <ajava2424@gmail.com>
2024-08-23 12:50:17 -0400

    4.10

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert,ErrorAlert, WarningAlert } from './components/Alert';
-import './App.css';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
+import './App.css';
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -47,7 +48,10 @@ const App = () => {
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents currentNOE = {currentNOE} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <EventGenresChart events={events} /> 
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events}/>
     </div>
   );
