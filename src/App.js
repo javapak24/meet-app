25e90d5a0e6a62973be01c8d1bd4bf4d5eb979c0
javapak24 <ajava2424@gmail.com>
2024-08-23 12:06:02 -0400

    msg

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     // const filteredEvents = currentCity === "See all cities" ?
     //   allEvents :
     //   allEvents.filter(event => event.location === currentCity)
-    filteredEvents = allEvents;
+    const filteredEvents = allEvents;
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
